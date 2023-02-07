08d3c6d9206544ca8aacfb4eb3ec40182e5745f8
DaiweiChen <dworign@outlook.com>
2023-02-07 14:01:01 +0800

    环境yaml修改完成

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:jtjs/main.dart';
 
-import 'package:my_fisrt_flutter_app/main.dart';
+// import 'package:my_fisrt_flutter_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
