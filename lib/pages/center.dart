342448f6d49bd22b5cd45f933c952536f04b8d41
DaiweiChen <dworign@outlook.com>
2023-02-07 17:54:50 +0800

    个人中心

--- lib/pages/center.dart
+++ lib/pages/center.dart
@@ -9,6 +9,7 @@ import 'package:permission_handler/permission_handler.dart';
 
 import './location.dart';
 import './user_center/user_information.dart';
+
 class CenterPage extends StatefulWidget {
   const CenterPage({Key? key}) : super(key: key);
 
@@ -20,110 +21,234 @@ class _CenterPageState extends State<CenterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(250, 239, 249, 255),
       appBar: AppBar(
         // leading: Icon(Icons.arrow_back_outlined),
         title: const Text('智慧公交——主页'),
       ),
       body: Center(
-        child: Column(
+        child: ListView(
           // mainAxisAlignment: MainAxisAlignment.center,
           // crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(40),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 85,
-                      height: 85,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(42)),
-                      child: ClipOval(
-                        child: Image.asset(
-                          'assets/images/lake.jpg',
-                          width: 85,
-                          height: 85,
-                          fit: BoxFit.cover,
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              margin: const EdgeInsets.all(10),
+              color: Colors.greenAccent,
+              elevation: 10,
+              shadowColor: Colors.grey,
+              child: Container(
+                height: 100,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 85,
+                        height: 85,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(42)),
+                        child: ClipOval(
+                          child: Image.asset(
+                            'assets/images/lake.jpg',
+                            width: 85,
+                            height: 85,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    const Padding(padding: EdgeInsets.all(10)),
-                    TextButton(onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const UserInfoPage()));
-                    },
-                    child: const Text("cdw"),
-                    ),
-                    const Padding(padding: EdgeInsets.all(30)),
-                    IconButton(
-                        onPressed: () {}, icon: const Icon(Icons.chat_outlined))
-                  ]),
+                      const Padding(padding: EdgeInsets.all(10)),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const UserInfoPage()));
+                        },
+                        child: const Text(
+                          "cdw",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900, fontSize: 26),
+                        ),
+                      ),
+                      const Padding(padding: EdgeInsets.all(30)),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.chat_outlined))
+                    ]),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.all(10),
+                  color: const Color.fromARGB(250, 250, 252, 254),
+                  elevation: 10,
+                  shadowColor: const Color.fromARGB(250, 231, 241, 251),
+                  child: Container(
+                      height: 60,
+                      margin: const EdgeInsets.all(10),
+                      child: TextButton.icon(
+                        icon: const Icon(Icons.history),
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(120, 60)),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return const LocationPage();
+                          }));
+                        },
+                        label: const Text(
+                          '历史记录',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      )),
+                ),
+                const Padding(padding: EdgeInsets.all(10)),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.all(10),
+                  color: const Color.fromARGB(250, 250, 252, 254),
+                  elevation: 10,
+                  shadowColor: const Color.fromARGB(250, 231, 241, 251),
+                  child: Container(
+                    height: 60,
                     margin: const EdgeInsets.all(10),
-                    child: ElevatedButton.icon(
-                      icon: const Icon(Icons.history),
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: const Size(120, 60)),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const LocationPage();
-                        }));
-                      },
-                      label: const Text('历史记录'),
-                    )),
-                Container(
-                    margin: const EdgeInsets.all(10),
-                    child: ElevatedButton.icon(
-                      icon: const Icon(Icons.star_border),
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: const Size(120, 60)),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const LocationPage();
-                        }));
-                      },
-                      label: const Text('我的收藏'),
-                    )),
+                    child: TextButton.icon(
+                        icon: const Icon(
+                          Icons.star_border,
+                          color: Color.fromARGB(250, 74, 185, 255),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(120, 60)),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return const LocationPage();
+                          }));
+                        },
+                        label: const Text(
+                          '我的收藏',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        )),
+                  ),
+                ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                    margin: const EdgeInsets.all(10),
-                    child: ElevatedButton.icon(
-                      icon: const Icon(Icons.account_balance_wallet_outlined),
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: const Size(120, 60)),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const LocationPage();
-                        }));
-                      },
-                      label: const Text('钱包余额'),
-                    )),
-                Container(
-                    margin: const EdgeInsets.all(10),
-                    child: ElevatedButton.icon(
-                      icon: const Icon(Icons.support_agent_outlined),
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: const Size(120, 60)),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return const LocationPage();
-                        }));
-                      },
-                      label: const Text('客服中心'),
-                    )),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.all(10),
+                  color: const Color.fromARGB(250, 250, 252, 254),
+                  elevation: 10,
+                  shadowColor: const Color.fromARGB(250, 231, 241, 251),
+                  child: Container(
+                      margin: const EdgeInsets.all(10),
+                      child: TextButton.icon(
+                        icon: const Icon(Icons.account_balance_wallet_outlined),
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(120, 60)),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return const LocationPage();
+                          }));
+                        },
+                        label: const Text('钱包余额',
+                            style: TextStyle(
+                              color: Colors.black,
+                            )),
+                      )),
+                ),
+                const Padding(padding: EdgeInsets.all(10)),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.all(10),
+                  color: const Color.fromARGB(250, 250, 252, 254),
+                  elevation: 10,
+                  shadowColor: const Color.fromARGB(250, 231, 241, 251),
+                  child: Container(
+                      margin: const EdgeInsets.all(10),
+                      child: TextButton.icon(
+                        icon: const Icon(Icons.support_agent_outlined),
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(120, 60)),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return const LocationPage();
+                          }));
+                        },
+                        label: const Text('客服中心',
+                            style: TextStyle(
+                              color: Colors.black,
+                            )),
+                      )),
+                ),
               ],
+            ),
+            SizedBox(
+              height: 50,
+              child: Row(
+                children: [
+                  const Padding(padding: EdgeInsets.all(10)),
+                  const SizedBox(
+                    child: Text(
+                      "我创建的文件夹",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w900, fontSize: 15),
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(60)),
+                  SizedBox(
+                    height: 48,
+                    width: 48,
+                    child: TextButton.icon(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)))),
+                      label: const Text(""),
+                      icon: const Icon(Icons.add,),
+                      // child:  const Text("圆角"),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 48,
+                    width: 48,
+                    child: TextButton.icon(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)))),
+                      label: const Text(""),
+                      icon: const Icon(Icons.list,),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
--- lib/pages/user_center/user_information.dart
+++ lib/pages/user_center/user_information.dart
@@ -10,28 +10,205 @@ class UserInfoPage extends StatefulWidget {
 }
 
 class _UserInfoPageState extends State<UserInfoPage> {
-  List<int> _selectedIndex=[123,1,5];
-  DateTime dateTime = DateTime(2023, 2, 4);
+   DateTime dateTime = DateTime(2023, 2, 4);
+   List pickerChildren = [
+     "男",
+     "女",
+     "保密",
+   ];
+   int selectedValue = 0;
+  void _changeBirthState() {
+      DatePicker.showDatePicker(context,
+          dateFormat: "yyyy年 MM月 dd日", //通过空格设置滚轮数量
+          pickerTheme: const DateTimePickerTheme(
+            backgroundColor: Colors.white,
+              itemTextStyle: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+              //样式设置，可以设置确认按钮、取消按钮、标题等
+              cancel: Text("取消",style: TextStyle(fontSize: 14, color: Colors.blueAccent,fontWeight:FontWeight.w900)),
+              confirm:
+              Text("确认",
+                  style: TextStyle(fontSize: 14, color: Colors.blueAccent,fontWeight:FontWeight.w900))),
+          onConfirm: (dateTimes, selectedIndex) {
+            setState((){
+              dateTime = dateTimes;
+            });
+        // 确认按钮回调，获取选择结果 dateTime
+      });
+
+  }
+
+   void _didClickSelectedGender() {
+     showCupertinoModalPopup(
+         context: context,
+         builder: (BuildContext context) {
+           var controller =
+           FixedExtentScrollController(initialItem: selectedValue);
+           return Container(
+             // padding: EdgeInsets.all(10),
+             height: 250,
+             color: Colors.grey[200],
+             child: Column(
+               children: <Widget>[
+                 SizedBox(
+                   height: 40,
+                   // color: Colors.grey,
+                   child: Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                     children: <Widget>[
+                       TextButton(
+                         onPressed: () {
+                           Navigator.pop(context);
+                         },
+                         child: const Text("取消",style:TextStyle(fontSize: 14, color: Colors.blueAccent,fontWeight:FontWeight.w900),),
+                       ),
+                       TextButton(
+                         onPressed: () {
+                           Navigator.pop(context, controller.selectedItem);
+                         },
+                         child:const Text("确认",style: TextStyle(fontSize: 14, color: Colors.blueAccent,fontWeight:FontWeight.w900)),
+                       ),
+                     ],
+                   ),
+                 ),
+                 Expanded(
+                   child: DefaultTextStyle(
+                     style: const TextStyle(
+                       color: Colors.blueAccent,
+                       fontSize: 22,
+                     ),
+                     child: Container(
+                       height: 200,
+                       child: CupertinoPicker(
+                         scrollController: controller,
+                         // diameterRatio: 1.5,
+                         // offAxisFraction: 0.2, //轴偏离系数
+                         // useMagnifier: true, //使用放大镜
+                         // magnification: 1.5, //当前选中item放大倍数
+                         itemExtent: 32, //行高
+                         // backgroundColor: Colors.amber, //选中器背景色
+                         onSelectedItemChanged: (value) {
+                           print("value = $value, 性别：${pickerChildren[value]}");
+                         },
+                         children: pickerChildren.map((data) {
+                           return Center(
+                             child: Text(data,style: const TextStyle(fontWeight: FontWeight.bold
+                             ),),
+                           );
+                         }).toList(),
+                       ),
+                     ),
+                   ),
+                 ),
+               ],
+             ),
+           );
+         }).then((value) {
+       if (value != selectedValue && value != null) {
+         setState(() {
+           selectedValue = value;
+         });
+       }
+     });
+   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 245, 245, 245),
+
       // backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text("个人中心"),
       ),
       body: ListView(
         children: <Widget>[
-          // 这段做账号绑定
-
+          const SizedBox(height: 10,),
           Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20),
             ),
             margin: const EdgeInsets.all(5),
-            color: Colors.white60,
-            shadowColor: Colors.white12,
+            color: Colors.white,
+            shadowColor: Colors.white10,
             elevation: 20,
+            child: Column(
+              children: [
+                // 这段做个人信息内容
+                ListTile(
+                  title: Text("头像"),
+                  trailing: ClipOval(
+                    child: Image.asset(
+                      'assets/images/lake.jpg',
+                      width: 60,
+                      height: 60,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const Divider(),
+                const ListTile(
+                  title: Text("姓名: cdw"),
+                ),
+
+               const Divider(),
+                const SizedBox(height: 10,),
+                Row(
+                  children:   [
+                const Text(
+                      "    性别:  ",
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                GestureDetector(
+                 onTap: _didClickSelectedGender,
+                 child: Text("${pickerChildren[selectedValue]}",style: const TextStyle(
+                   fontSize: 16,color: Colors.blue
+                 ),),
+                )
+                  ],
+                ),
+                const SizedBox(height: 10,),
+                const Divider(),
+                Column(
+                  children: [
+                    SizedBox(
+                      child: Row(
+                        children: [
+                          const Text(
+                            "    出生日期:",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          CupertinoButton(
+                              onPressed: _changeBirthState,
+                              child:
+                              Text('$dateTime'.toString().substring(0,10))),
+
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const Divider(),
+                const ListTile(
+                  title: Text("个性签名: 不知道写什么东西进去"),
+                ),
+              ],
+            ),
+          ),
+          // 这段做账号绑定
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: const EdgeInsets.all(5),
+            color: Colors.white,
+            shadowColor: Colors.white10,
+            elevation: 10,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: const [
@@ -45,74 +222,7 @@ class _UserInfoPageState extends State<UserInfoPage> {
               ],
             ),
           ),
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            margin: const EdgeInsets.all(5),
-            color: Colors.white60,
-            shadowColor: Colors.white12,
-            elevation: 20,
-            child: Column(
-              children: [
-                // 这段做个人信息内容
-                ListTile(
-                  title: Text("姓名: cdw"),
-                ),
 
-                Divider(),
-                Row(
-                  children: [
-                      Text("    性别:",style: TextStyle(
-                        fontSize: 16,
-                      ),),
-                  ],
-                ),
-
-                Divider(),
-                Column(
-                  children: [
-                    SizedBox(
-                      child:
-                      Row(
-                        children: [
-                          Text("    出生日期:",style: TextStyle(
-                            fontSize: 16,
-                          ),) ,
-                          CupertinoButton(
-                              onPressed: () {
-                                DatePicker.showDatePicker(context,
-                                    dateFormat: "yyyy年 MM月 dd日", //通过空格设置滚轮数量
-                                    pickerTheme: const DateTimePickerTheme(
-                                      //样式设置，可以设置确认按钮、取消按钮、标题等
-                                        confirm: Text("确认",
-                                            style: TextStyle(
-                                                fontSize: 14,
-                                                color: Colors.blue))), onConfirm:
-                                        ( dateTime, selectedIndex) {
-                                  _selectedIndex = selectedIndex;
-                                      // 确认按钮回调，获取选择结果 dateTime
-                                    }
-                                    );
-                              },
-                            child:Text((new DateTime(_selectedIndex[0]+1900,_selectedIndex[1]+1,_selectedIndex[2]+1)).toString()) ,)
-                        ],
-                      ),
-                    )
-
-
-
-
-                  ],
-                ),
-
-                Divider(),
-                ListTile(
-                  title: Text("个性签名: 不知道写什么东西进去"),
-                ),
-              ],
-            ),
-          ),
         ],
       ),
     );
